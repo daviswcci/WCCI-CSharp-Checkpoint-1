b4bae05ac6a742bbe7da3da8df34709e813f589f
Davis Murphy <79462946+daviswcci@users.noreply.github.com>
2021-09-21 11:52:26 -0400

    added proj files back

---
+++ WCCI CSharp Checkpoint 1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace WCCI_CSharp_Checkpoint_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSTRUCTIONS
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            // Complete the following exercises, writing code beneath each exercise's instructions. Treat this as an opportunity to test what you know!
+            // Do a first pass without outside code/resources, and then go back to attempt the ones you weren't sure about for completion. Reach out if you have any questions! 
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Create a comment below with your favorite pizza topping
+
+            // Do the same, but with a multi-line comment
+
+            // Declare a string variable named 'firstName'
+
+            // Assign a value of "Davis" to 'firstName'
+
+            // Declare a variable named 'lastName' and assign it a value of "Murphy" in one line
+
+            // Using string concatenation, put 'firstName' and 'lastName' together, and assign the result to a new variable named 'fullName'
+
+            // Declare two variables, 'wholeNumber' and 'decimalNumber' that have types fitting for their names. Assign both a value of 15.
+
+            // Print out 'fullName' to the console.
+
+            // Read a user's input from the console, and store the result of that response as a variable named 'response'. No leading question is needed.
+
+            // Write an 'if' statement that prints out "That's a long response!" if the length of 'response' is greater than 15. Add an else statement that prints "What was that?" instead
+
+            // Create a 'switch' statement that switches based on the user's input. Do this without creating an additional variable. (Hint: Coding is just substitution..). Then, create cases that print different messages.
+
+            // Convert a user's input to an integer, and store that value in a variable called 'numberResponse'
+
+            // Create a 'for' loop that loops 15 times, printing out the numbers 1-15 (or 0-14)
+
+            //Create a variable called 'loopIndex' that has an initial value of 47. Then, create a 'while' loop that decrements 'loopIndex' and continues to run while 'loopIndex' is greater than 13
+
+            // Use the decrement assignment operator to subtract 5 from 'loopIndex'
+
+            // Create a boolean array of size 10 named 'numbers'
+
+            // Assign the value of the 4th item in our array to be 'false' (Hint: Is 4 really 4?)
+
+            // Print the 4th item in our array to the console
+
+            // Create a variable named 'names' that is a string array of size 3 with the values "Davis", "Gavin", and "Carlos" (Hint: We can use {} to hard code those values... But how?)
+
+            // Comment your response: Can we run 'Console.WriteLine(names[3])' ?
+
+            // Create a list of strings called 'nameList'. 
+
+            // Use the '.add()' method to put "Davis", "Gavin", and "Carlos" into the list
+
+            // Create a foreach loop to print every item in 'nameList' to the user
+
+            // Create a string dictionary called 'stringDict' that uses doubles as the key
+
+            // Add "Davis" "Gavin" and "Carlos" to the dictionary with key values of 4.7, 1.3, and 9.9 respectively
+
+        }
+    }
+
+    // In this file, create a new class called 'Kitchen' that has the following properties and methods:
+
+    // Create a boolean called 'HasMilk', a string called 'Name' and an integer called 'Spatulas'
+
+    //Create a public static method named 'Toast' that accepts a string called 'bread' as input. First, print the message "Toasting..." to the user. Then, check to see if the string has a value of "buttered". If it does, return "toast". If not, return "dry bread"
+
+    // Create a public static method named 'Burn' that accepts a string called 'bread' as input. First, print the message "Toasting..." to the user.  Then print the message "Burnt." to the user, and return.
+
+    // Back inside our Program's main method, run 'Toast("buttered")' and 'Burn("buttered")' 
+
+    // Complete a hard sudoku puzzle
+
+    // Have a great day, and revel in this new experience! Additional things to explore: Interfaces/Abstract Classes, additional Console features (beep and music), and packages
+}
